3477b317c9a6f50550bed8b72324cd13e900dc20
MelodyDuplaix <melo.surseine@gmail.com>
2023-09-07 11:42:10 +0200

    ajout d'un bouton reset des filtres d'affichages

--- Pages/ajouter.py
+++ Pages/ajouter.py
@@ -22,22 +22,29 @@ st.info(body="Tous les champs sont obligatoires.", icon="❗")
 
 # Formulaire
 with st.form("Formulaire d'ajout", clear_on_submit=True):
+    
     prenom = st.text_area("Prénom", max_chars=25, placeholder="Pas de prénom", height=1, key="prenom")
+    
     nom = st.text_area("Nom", max_chars=25, placeholder="Pas de nom", height=1, key="nom")
+    
     mail = st.text_area("Mail", max_chars=50, placeholder="Pas de mail", height=1, key="mail")
     if mail and( not "@" in mail or " " in mail):
         st.write(":red[Le mail n'est pas valide !]")
+        
     today = datetime.datetime.now()
     date_de_naissance = st.date_input(
         "Date de naissance", max_value=datetime.date((today.year-18), 1, 1), min_value= datetime.date((today.year-100), 1, 1),
-        format="YYYY.MM.DD", value= datetime.date((today.year-30), 1, 1), key="date_naissance"
+        format="DD/MM/YYYY", value= datetime.date((today.year-30), 1, 1), key="date_naissance"
     )
+
     genre = st.selectbox("Genre", ("F","M"), help="Homme : H Femme : F", key="genre")
     date_arrive = st.date_input(
         "Date d'entrée dans l'entreprise", min_value= datetime.date((today.year-60), 1, 1),
-        format="YYYY.MM.DD", key="date_arrive"
+        format="DD/MM/YYYY", key="date_arrive"
     )
+    
     liste_ville = récupérer_liste_ville()
+    
     localisation = st.selectbox("Ville",liste_ville, key="localisation")
     
     
--- Pages/modifier.py
+++ Pages/modifier.py
@@ -43,7 +43,7 @@ with st.form("Formulaire d'ajout", clear_on_submit=False):
     date_de_naissance= date_obj = datetime.datetime.strptime(ligne_individu["date_naissance"].values[0], "%Y-%m-%d")
     date_de_naissance = st.date_input(
         "Date de naissance", max_value=datetime.date((today.year-18), 1, 1), min_value= datetime.date((today.year-100), 1, 1),
-        format="YYYY.MM.DD", key="date_naissance", value=date_de_naissance
+        format="DD/MM/YYYY", key="date_naissance", value=date_de_naissance
     )
     if ligne_individu["genre"].values[0] == "F":
         genres_possible = ("F","M")
@@ -55,7 +55,7 @@ with st.form("Formulaire d'ajout", clear_on_submit=False):
     date_entree= date_obj = datetime.datetime.strptime(ligne_individu["date_arrivee"].values[0], "%Y-%m-%d")
     date_arrive = st.date_input(
         "Date d'entrée dans l'entreprise", min_value= datetime.date((today.year-60), 1, 1),
-        format="YYYY.MM.DD", key="date_arrive", value=date_entree
+        format="DD/MM/YYYY", key="date_arrive", value=date_entree
     )
     
     liste_ville = récupérer_liste_ville()
@@ -66,7 +66,7 @@ with st.form("Formulaire d'ajout", clear_on_submit=False):
     table_ville = récupérer_tableau_ville()
     localisation_id = table_ville[table_ville["nom"]==localisation]["id"].values
 
-    date_sortie = st.date_input("Date de sortie", format="YYYY.MM.DD", value=datetime.date(2100, 1, 1), min_value= datetime.date((today.year-60), 1, 1), help="Veuillez laissez la date  " + datetime.date(2100, 1, 1).strftime("%Y-%m-%d") + "  si la date est nulle")
+    date_sortie = st.date_input("Date de sortie", format="DD/MM/YYYY", value=datetime.date(2100, 1, 1), min_value= datetime.date((today.year-60), 1, 1), help="Veuillez laissez la date  " + datetime.date(2100, 1, 1).strftime("%Y-%m-%d") + "  si la date est nulle")
     if date_sortie == datetime.date(2100, 1, 1):
         date_sortie = None
     else:
